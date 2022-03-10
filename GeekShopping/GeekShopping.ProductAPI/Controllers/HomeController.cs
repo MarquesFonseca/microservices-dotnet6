373b9f4953100f61c41dedffef5574c523403fa2
Marques Silva Fonseca <marques-fonseca@hotmail.com>
2022-03-10 14:19:34 -0300

    testes

---
+++ GeekShopping/GeekShopping.ProductAPI/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Repository.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/vi/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new
+                ArgumentException(nameof(repository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+        {
+            var products = await _repository.FindAll();
+            if (products == null) return NotFound();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
+        {
+            var product = await _repository.FindById(id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+    }
+}
