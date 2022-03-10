43c2e82f687fbb89617af789441870d410347f2f
Marques Silva Fonseca <marques-fonseca@hotmail.com>
2022-03-10 14:30:54 -0300

    -

---
+++ GeekShopping/Teste.API/Repository/Interface/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Models;
+
+namespace GeekShopping.ProductAPI.Repository.Interface
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<ProductVO> FindById(long id);
+        Task<ProductVO> Create(ProductVO productVO);
+        Task<ProductVO> Update(ProductVO productVO);
+        Task<bool> Delete(long id);
+    }
+}
