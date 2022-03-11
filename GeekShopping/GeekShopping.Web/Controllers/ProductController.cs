34b33a662b825ed3b3bb64eb4f7f40932fcf2b3e
Marques Silva Fonseca <marques-fonseca@hotmail.com>
2022-03-11 13:06:30 -0300

    Implementation Create, update, Delete at Views

--- GeekShopping/GeekShopping.Web/Controllers/ProductController.cs
+++ GeekShopping/GeekShopping.Web/Controllers/ProductController.cs
@@ -19,5 +19,61 @@ namespace GeekShopping.Web.Controllers
             var products = await _productService.FindAllProduct();
             return View(products);
         }
+
+        public async Task<IActionResult> ProductCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductCreate(ProductModel productModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.CreateProduct(productModel);
+                if (response != null)
+                    return RedirectToAction(nameof(ProductIndex));
+            }
+            return View(productModel);
+        }
+
+        public async Task<IActionResult> ProductUpdate(long id)
+        {
+            var productModel = await _productService.FindProductById(id);
+            if (productModel != null)
+                return View(productModel);
+            return NotFound();
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductUpdate(ProductModel productModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.UpdateProduct(productModel);
+                if (response != null)
+                    return RedirectToAction(nameof(ProductIndex));
+            }
+            return View(productModel);
+        }
+
+        public async Task<IActionResult> ProductDelete(int id)
+        {
+            var productModel = _productService.FindProductById(id);
+            if (productModel != null)
+                return View(productModel);
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductDelete(ProductModel productModel)
+        {
+            var response = await _productService.DeleteProductById(productModel.Id);
+            if (response)
+                return RedirectToAction(nameof(ProductIndex));
+            return View(productModel);
+        }
+
     }
 }
